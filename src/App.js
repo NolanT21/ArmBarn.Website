b3d376a4bbb66ec1cee16c2fb713adadcf8c855d
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-09-24 14:10:30 -0400

    Removed routers

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
 
   return(
     <>
-      <BrowserRouter basename = "/ArmBarn.Website">
+      {/* <BrowserRouter basename = "/ArmBarn.Website">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
           <Route path="/TermsOfService" element={<TermsOfService />} />
           <Route path="/HelpSupport" element={<HelpSupport />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <HomePage/>
     </>
   );
 
