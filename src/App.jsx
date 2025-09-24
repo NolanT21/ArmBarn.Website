5b0538b56fc727d97d53f24114b41913c7f09732
NolanT21 <130325092+NolanT21@users.noreply.github.com>
2025-09-24 12:10:02 -0400

    Reapply "Website 3.0 upload"
    This reverts commit 147722d05a5b763b0768df42b14fe47e3131e847.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import HomePage from "./HomePage";
+import PrivacyPolicy from "./PrivatePolicy";
+import TermsOfService from "./TermsOfService";
+import HelpSupport from "./HelpSupport";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/TermsOfService" element={<TermsOfService />} />
+          <Route path="/HelpSupport" element={<HelpSupport />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+
+}
+
+export default App
